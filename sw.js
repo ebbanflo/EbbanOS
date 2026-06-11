9c8483a7ef67f003124caaf9e00b95eb9d931653
ebbanflo <57416408+ebbanflo@users.noreply.github.com>
2026-06-11 13:59:12 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'secondbrain-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(hit => hit ||
+      fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+    )
+  );
+});
